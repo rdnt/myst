fa148836f87e8570d7dd27310dd610bb23704853
Tasos papalyras <tasos@sht.gr>
2020-03-28 01:48:11 +0200

    Restyle

--- vue/src/components/Login.vue
+++ vue/src/components/Login.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="login" :class="{ hide: login == true }">
+  <div id="login" :class="{ show: !loggedIn }">
     <form>
       <img class="logo" src="/assets/images/vault.svg" alt="" />
       <input
@@ -15,12 +15,14 @@
 </template>
 
 <script>
+import { mapState } from "vuex";
 export default {
   data() {
-    return {
-      login: true
-    };
+    return {};
   },
+  computed: mapState({
+    loggedIn: state => state.loggedIn
+  }),
   mounted() {
     setTimeout(() => {
       this.login = true;

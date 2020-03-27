d5feff85da028ae757116bfde773956e157dcc3f
Tasos papalyras <tasos@sht.gr>
2020-03-28 01:45:43 +0200

    WebAssembly support for end to end encryption/decryption of the keystore

--- testwasm/main.go
+++ testwasm/main.go
@@ -5,23 +5,58 @@ package main
 //go:generate cp $GOROOT/misc/wasm/wasm_exec.js .
 
 import (
-	"fmt"
+	"encoding/hex"
 	"syscall/js"
+
+	"github.com/sht/myst/go/keystore"
 )
 
-
 func main() {
-	// document := js.Global().Get("document")
-	// canvas := document.Call("getElementById", "canvas")
-	// fmt.Println("from wasm:")
-	// fmt.Println(document)
-
-	js.Global().Set("test", js.FuncOf(test))
-
+	js.Global().Set("keystore", js.ValueOf(map[string]interface{}{
+		"_decrypt": js.FuncOf(Decrypt),
+		"_encrypt": js.FuncOf(Encrypt),
+	}))
+	// js.Global().Set("decryptKeystore", js.FuncOf(Decrypt))
+	// js.Global().Set("encryptKeystore", js.FuncOf(Encrypt))
 	select {}
 }
 
-func test(this js.Value, inputs []js.Value) interface{}  {
-	fmt.Println("test invoked")
-	return nil
+func Decrypt(this js.Value, args []js.Value) interface{} {
+	if len(args) == 3 {
+		raw := []byte(args[0].String())
+		pass := args[1].String()
+		cb := args[2]
+		b := make([]byte, hex.DecodedLen(len(raw)))
+		_, err := hex.Decode(b, raw)
+		if err != nil {
+			cb.Invoke(nil, err.Error())
+			return nil
+		}
+		json, err := keystore.Decrypt(b, []byte(pass))
+		if err != nil {
+			cb.Invoke(nil, err.Error())
+			return nil
+		}
+		cb.Invoke(string(json), nil)
+		return nil
+	}
+	return false
+}
+
+func Encrypt(this js.Value, args []js.Value) interface{} {
+	if len(args) == 3 {
+		json := args[0].String()
+		pass := args[1].String()
+		cb := args[2]
+		b, err := keystore.Encrypt([]byte(json), []byte(pass))
+		if err != nil {
+			cb.Invoke(nil, err.Error())
+			return nil
+		}
+		h := make([]byte, hex.EncodedLen(len(b)))
+		hex.Encode(h, b)
+		cb.Invoke(string(h), nil)
+		return nil
+	}
+	return false
 }

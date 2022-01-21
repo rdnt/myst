354d50130f59f9fca419a79a9d65998f1f725dd3
Tasos Papalyras <tasos@rdnt.dev>
2022-01-22 00:45:18 +0200

    New onboarding UI

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -6,25 +6,21 @@
   <router-view/>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<!--<style lang="scss">-->
+<!--#app {-->
+<!--  //color: #f00;-->
+<!--}-->
 
-#nav {
-  padding: 30px;
+<!--#nav {-->
+<!--  padding: 30px;-->
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+<!--  a {-->
+<!--    font-weight: bold;-->
+<!--    color: #2c3e50;-->
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<!--    &.router-link-exact-active {-->
+<!--      color: #42b983;-->
+<!--    }-->
+<!--  }-->
+<!--}-->
+<!--</style>-->
--- ui/src/components/InitializeKeystoreFullscreenModal.vue
+++ ui/src/components/InitializeKeystoreFullscreenModal.vue
@@ -4,32 +4,42 @@
       <div class="modal-overlay" v-on:click="hide" />
       <div class="modal-content">
         <div class="setup">
-          <div class="header">
-            <h4>First time setup</h4>
-            <h6>It seems that this is the first time you are opening Myst on this device.</h6>
-            <h6>Let's choose a master password. It will be used to securely store your secrets.</h6>
+          <h4>First time setup</h4>
+          <br/>
+          <div class="separator"/>
+          <br/><br/><br/><br/>
+          <h6 v-if="step === 1">Let's choose a name for your first keystore. Keystores help organize your secrets into groups.</h6>
+          <h6 v-if="step === 2">Your selected keystore name:</h6>
+          <br v-if="step === 1" />
+          <div class="input">
+            <label>Keystore name</label>
+            <input class="password-input" :class="step === 2 ? 'selected': ''" placeholder="Keystore name" />
           </div>
-          <label>
-            <input spellcheck="false" class="password-input" placeholder="master password"/>
-          </label>
-          <div class="footer">
-            <button class="step-button">Next</button>
-          </div>
+
+          <template v-if="step === 1">
+            <div class="footer">
+              <button class="step-button" @click="step = 2">Next</button>
+<!--              <button class="step-button green">Create</button>-->
+            </div>
+          </template>
+
+          <template v-else>
+            <br/><br/><br/><br/><br/>
+            <h6>Your keystores will be encrypted using a master password. The security of your secrets will depend on its
+              complexity. Choose wisely.</h6>
+            <br/>
+            <div class="input">
+              <label>Master password</label>
+              <expanding-textarea class="password-input" placeholder="Master password" @change="masterPasswordChanged" />
+            </div>
+            <br/>
+            <div class="footer">
+              <!--<button class="step-button">Cancel</button>-->
+              <button class="step-button green">Create</button>
+            </div>
+          </template>
 
         </div>
-
-        <!--        <div class="modal-header">-->
-<!--          <slot name="header"></slot>-->
-<!--        </div>-->
-<!--        <div class="modal-body">-->
-<!--          <slot></slot>-->
-<!--        </div>-->
-<!--        <div class="modal-footer">-->
-<!--          <button class="button" v-on:click="hide">-->
-<!--            Cancel-->
-<!--          </button>-->
-<!--          <slot name="footer"></slot>-->
-<!--        </div>-->
       </div>
     </div>
   </transition>
@@ -37,26 +47,76 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import ExpandingTextarea from '@/components/ExpandingTextarea.vue'
 
 export default defineComponent({
   name: 'InitializeKeystoreFullscreenModal',
+  components: { ExpandingTextarea },
   props: ['show'],
+  data: () => ({
+    keystoreName: '',
+    masterPassword: '',
+    step: 1
+  }),
   methods: {
     hide () {
       this.$emit('hide')
+    },
+    keystoreNameChanged (name: string) {
+      this.keystoreName = name
+    },
+    masterPasswordChanged (password: string) {
+      this.masterPassword = password
     }
   }
 })
 </script>
 
 <style scoped lang="scss">
-$bg: #24292e;
+$bg: #0a0e11;
+$accent: #00edb1;
+//$text-color: #f4f8fb;
+$text-color: #fff;
 
 @import url('https://rsms.me/inter/inter.css');
 * {
   font-family: 'Inter', sans-serif;
   font-weight: 300;
   font-size: 100%;
+  color: $text-color;
+  line-height: 1;
+}
+
+textarea {
+  margin: 0;
+  padding: 0;
+  border: none;
+  outline: none;
+  width: 100%;
+  resize: none;
+  line-height: 1;
+  padding: 15px 16px !important;
+}
+
+.input {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+
+  label {
+    flex-basis: 100%;
+    margin-bottom: 10px;
+    opacity: .8;
+    padding: 4px 8px;
+    display: none;
+  }
+}
+
+.separator {
+  width: 100px;
+  height: 2px;
+  background-color: #31363d;
+  margin: 0 auto;
 }
 
 input {
@@ -66,9 +126,11 @@ input {
   background-color: transparent;
   color: #fff;
   outline: none;
+  width: 100%;
 }
 
 h4 {
+  font-weight: 700;
   font-size: 1.8rem;
   margin: 0;
 }
@@ -76,6 +138,7 @@ h4 {
 h6 {
   font-size: 1rem;
   margin: 0;
+  line-height: 1.4;
 }
 
 button {
@@ -87,13 +150,25 @@ button {
 }
 
 .step-button {
+  font-size: 1.1rem;
+  //text-transform: uppercase;
+  font-weight: 500;
+  padding: 14px 30px;
+  border-radius: 5px;
+  margin: 0 5px;
+  background-color: rgba(#fff, .05);
   color: #fff;
-  font-size: 1rem;
-  font-weight: 400;
-  padding: 8px 0;
+  //text-decoration: underline;
+  //text-decoration-color: rgba(#fff, .25);
+  //text-underline-offset: 2px;
+
+  &.green {
+    background-color: #0d2322;
+    color: #56e9b6;
+  }
 
   &:hover {
-    text-decoration: underline;
+    //text-decoration: underline;
   }
 }
 
@@ -103,44 +178,68 @@ button {
   left: 50%;
   transform: translate(-50%, -50%);
   color: #e1e4e8;
+  width: 480px;
 
   .header {
+    position: relative;
     margin-bottom: 60px;
+    width: 500px;
+    box-sizing: border-box;
   }
 
   .footer {
     margin-top: 60px;
     display: flex;
-    justify-content: flex-end;
+    justify-content: center;
+
   }
 
   h4 {
     font-size: 1.8rem;
-    margin-bottom: 30px;
+    margin-bottom: 12px;
+    text-align: center;
   }
 
   h6 {
-    font-size: 1rem;
+    font-size: 1.1rem;
     margin-bottom: 6px;
+    font-weight: 300;
   }
 }
 
 .password-input {
-  border-bottom: 2px solid #3b4048;
-  font-size: 2rem;
-  padding: 10px 0;
-  width: 100%;
-  caret-color: #9ecbff;
+  display: block;
+  //border-bottom: 2px solid rgba($accent, .1);
+  font-size: 1.1rem;
+  font-weight: 400;
+  //width: 100%;
+  caret-color: $accent;
+  box-sizing: border-box;
+  background-color: rgba(#abc, .05);
+  border-radius: 5px;
+  padding: 15px 16px;
+  color: #fff;
+  line-height: 1.5;
+  overflow: hidden;
+
+  &.selected {
+    background: transparent;
+    font-weight: 600;
+    color: $accent;
+    padding: 0;
+    height: auto;
+    font-size: 1.4rem;
+  }
 
   &::placeholder {
-    color: #68737e;
+    color: lighten(#68737e, 5%);
   }
 
   &:focus {
-    border-bottom: 2px solid lighten(#3b4048, 10%);
+    //border-bottom: 2px solid lighten(#3b4048, 10%);
 
     &::placeholder {
-      color: lighten(#68737e, 10%);
+      color: lighten(#68737e, 15%);
     }
   }
 }

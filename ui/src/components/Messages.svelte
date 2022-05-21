8f30c568bb4e6ad9f989e1f790d064617988882b
Tasos Papalyras <tasos@rdnt.dev>
2022-05-22 01:37:00 +0300

    Messages component

---
+++ ui/src/components/Messages.svelte
@@ -0,0 +1,57 @@
+<script lang="ts">
+  import {slide} from 'svelte/transition';
+  import {messages, MessageType} from "../stores/messages"
+</script>
+
+{#if $messages.length > 0}
+  <div class="messages">
+    {#each $messages as message}
+      <div class="message"
+           class:info={message.type === MessageType.Info}
+           class:error={message.type === MessageType.Error}
+           transition:slide>
+        <div class="title">{message.title}</div>
+        {#if message.message}
+          <div class="msg">{message.message}</div>
+        {/if}
+      </div>
+    {/each}
+  </div>
+{/if}
+
+
+<style lang="scss">
+  .messages {
+    position: fixed;
+    bottom: 0;
+    right: 0;
+    margin: 20px;
+  }
+
+  .message {
+    margin-top: 10px;
+    border-radius: 4px;
+    padding: 14px 20px;
+
+    &.info {
+      background-color: #002e23;
+      color: #00edb1;
+    }
+
+    &.error {
+      background-color: #2e2020;
+      color: #ff9999;
+    }
+
+    .title {
+      font-weight: 600;
+      font-size: 1.1rem;
+    }
+
+    .msg {
+      margin-top: 4px;
+      font-weight: 400;
+      font-size: 1rem;
+    }
+  }
+</style>

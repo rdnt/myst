d10c966103f61cb2ade2345906fc43ed7995d30e
Tasos Papalyras <tasos@rdnt.dev>
2023-06-02 00:38:54 +0300

    Remove debug log, disable favicon for edit entry modal

--- ui/src/components/EditEntryModal.svelte
+++ ui/src/components/EditEntryModal.svelte
@@ -46,10 +46,10 @@
 <form class="edit-entry-modal" on:submit|preventDefault={submit}>
   <Modal bind:show>
     <div class="modal-header" slot="header">
-      <div class="image">
-        <img alt={entry.website}
-             src="https://www.nicepng.com/png/full/52-520535_free-files-github-github-icon-png-white.png"/>
-      </div>
+<!--      <div class="image">-->
+<!--        <img alt={entry.website}-->
+<!--             src="https://www.nicepng.com/png/full/52-520535_free-files-github-github-icon-png-white.png"/>-->
+<!--      </div>-->
 
       <div class="title">
         <span class="website">{entry.website}</span>

c7074c71eb11f6134609de6e02115cc1c91d9d0a
Tasos Papalyras <tasos@rdnt.dev>
2023-06-11 00:31:52 +0300

    Refine user domain entity

--- src/client/application/domain/user/user.go
+++ src/client/application/domain/user/user.go
@@ -9,17 +9,18 @@ type User struct {
 	Id        string
 	Username  string
 	PublicKey []byte
+	// SharedSecret is the secret that is shared between the logged in user
+	// and this user. It is used to present the hashicon for the verification
+	// process.
 	// TODO: do not expose the shared secret directly, hash it and store
-	// the hash internally on the enclave for each user that has an invitation
-	// accepted/finalized.
+	//  the hash internally on the enclave for each user that has an invitation
+	//  accepted/finalized.
 	SharedSecret []byte
-	Verified     bool
 }
 
 func (u User) String() string {
 	return fmt.Sprintln(
 		u.Id, u.Username,
 		base64.StdEncoding.EncodeToString(u.PublicKey),
-		u.Verified,
 	)
 }
